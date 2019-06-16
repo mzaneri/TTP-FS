8b4444f00e47d60fb42ffa6d5ea8b0c59edea4ca
mzaneri <michaelzaneri@gmail.com>
2019-06-16 18:38:21 -0400

    added GET Method Functionality and HTML for signup using CSRF safe Flask Form package

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = "gbbsb54k6rzta5i75yAEt"
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,12 @@
 from app import app
+from app.forms import SignUp
+from flask import render_template
 
 @app.route('/')
 def blank():
     return "Starting Page"
+
+@app.route('/signup')
+def signup():
+    form = SignUp()
+    return render_template('signup.html', form=form)
