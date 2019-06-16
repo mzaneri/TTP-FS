59dae9e2e79d0874eb64c885fe6808849d1fec6c
mzaneri <michaelzaneri@gmail.com>
2019-06-16 18:52:04 -0400

    Added logic for SignUp form submission (POST request) and confirmed form works as intended

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 from app import app
 from app.forms import SignUp
-from flask import render_template
+from flask import render_template, url_for, redirect
 
 @app.route('/')
 def blank():
     return "Starting Page"
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignUp()
+    if form.validate_on_submit():
+        print(form.name.data, form.email.data, form.password.data)
+        return redirect(url_for('blank'))
     return render_template('signup.html', form=form)
