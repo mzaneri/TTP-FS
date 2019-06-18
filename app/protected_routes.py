7fd1b3a64fba29c738836fe35099d7f7d0c60001
mzaneri <michaelzaneri@gmail.com>
2019-06-17 23:44:53 -0400

    Added user warnings for bad stock ticker, not having enough to make a purchase, trying to sell more than the user has

--- app/protected_routes.py
+++ app/protected_routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, flash
 from flask_login import current_user, login_user, login_required, logout_user
 from app import app, login_manager, bcrypt, conn, cur
 from app.sql import preparedTransactionLog, preparedCurrentPortfolio, preparedStock, preparedChangeBalance, preparedCurrentStock
@@ -30,20 +30,18 @@ def portfolio():
         ticker = form.ticker.data.lower()
         request_url = f"https://api.iextrading.com/1.0/stock/{ticker}/book"
         r = requests.get(request_url)
-        print(r.status_code)
         if r.status_code != 200:
+            flash("That ticker does not exist")
             return redirect(url_for("portfolio"))
         price = r.json()['quote']['latestPrice']
         print(form.quantity.data)
         quantity = int(form.quantity.data)
-        print(quantity)
-        if quantity == 0:
-            return redirect(url_for("portfolio"))
         transaction_cost = quantity * price
         if quantity < 0:
             cur.execute(preparedCurrentStock, (current_user.email, ticker))
             result = cur.fetchone()
             if result[0] is None or result[0] < (-1 * quantity):
+                flash("You can't sell more than you have")
                 return redirect(url_for("portfolio"))
             cur.execute(preparedStock, (current_user.email, ticker, price, quantity))
             conn.commit()
@@ -52,6 +50,7 @@ def portfolio():
             conn.commit()
             return redirect(url_for("portfolio"))
         elif transaction_cost > current_user.balance:
+            flash("You don't have enough money to buy this")
             return redirect(url_for("portfolio"))
         else:
             cur.execute(preparedStock, (current_user.email, ticker, price, quantity))
