775d6f0c8b91f40ff68adb373b5bf1699ce2d323
mzaneri <michaelzaneri@gmail.com>
2019-06-17 23:36:36 -0400

    Added proper notification to user for registration and signin issues. Also prevented signed in users from accessing register and signin views

--- app/authentication.py
+++ app/authentication.py
@@ -17,44 +17,53 @@ def user_loader(email):
 @login_manager.unauthorized_handler
 def unauthorized():
     flash("You must signin to access the last page you attempted to access")
-    return redirect(url_for("index"))
+    return redirect(url_for("signin"))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        flash("Can't register if you're already signed in")
+        return redirect(url_for("portfolio"))
     form = RegisterForm()
     if form.validate_on_submit():
         cur.execute(preparedUserInfo, (form.email.data,))
         result = cur.fetchone()
         if result is not None:
-            return render_template('register.html', form=form)
+            flash("Email is already in use")
+            return redirect(url_for('register'))
         pw_hash = bcrypt.generate_password_hash(form.password.data)
         newUserInfo = (form.name.data, form.email.data, pw_hash, 5000)
         cur.execute(preparedRegister, newUserInfo)
         conn.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('signin'))
     return render_template('register.html', form=form)
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
+    if current_user.is_authenticated:
+        flash("Can't signin if you're already signed in")
+        return redirect(url_for("portfolio"))
     form = SignInForm()
     if form.validate_on_submit():
         cur.execute(preparedUserInfo, (form.email.data,))
         result = cur.fetchone()
         if not result:
-            return redirect(url_for('index'))
+            flash("This user does not exist")
+            return redirect(url_for('register'))
         user = User(result)
         if bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
-            return redirect(url_for('secure'))
-        return redirect(url_for('index'))
+            return redirect(url_for('portfolio'))
+        flash("Incorrect password")
+        return redirect(url_for('signin'))
     return render_template('signin.html', form=form)
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('signin'))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('register'))
